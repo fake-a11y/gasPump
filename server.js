7c93de0903a7ebb77018aea53f697b8fab2b4313
fake-a11y <sguy7997@gmail.com>
2023-02-15 21:46:49 -0500

    Made changes to the UI/UX appearance and design

---
+++ server.js
@@ -0,0 +1,17 @@
+import express from 'express'
+import path from 'path'
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const app = express()
+const port = 9000
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.get('/', (request, response) => {
+  response.sendFile(path.join(__dirname, 'public/index.html'))
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
